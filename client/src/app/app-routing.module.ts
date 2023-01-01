7f4b953b623198b27260094435f77eb5fa92c056
Anthony Deville <adev08@outlook.com>
2023-01-01 19:06:15 +0100

    End of section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 /* Custom Components */
 import { HomeComponent } from 'src/app/home/home.component';
-import { ProductDetailsComponent } from 'src/app/shop/product-details/product-details.component';
-import { ShopComponent } from 'src/app/shop/shop.component';
+import { TestErrorComponent } from 'src/app/core/test-error/test-error.component';
+import { NotFoundComponent } from 'src/app/core/not-found/not-found.component';
+import { ServerErrorComponent } from 'src/app/core/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
   { path: '**', pathMatch: 'full', redirectTo: ''  }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 /* Custom Components */
 import { AppComponent } from 'src/app/app.component';
@@ -11,6 +11,9 @@ import { AppComponent } from 'src/app/app.component';
 /* Custom Modules */
 import { CoreModule } from 'src/app/core/core.module';
 import { HomeModule } from 'src/app/home/home.module';
+
+/* Custom Interceptors */
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { HomeModule } from 'src/app/home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    // Error interceptor
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
